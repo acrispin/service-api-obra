5a2d093994870f0e31e48af3f7d8b4b07a9fa291
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-02 13:47:12 -0500

    first commit

---
+++ src/main/java/com/unicon/api/serviceapiobra/ServiceApiObraApplication.java
@@ -0,0 +1,13 @@
+package com.unicon.api.serviceapiobra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiObraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApiObraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicon/api/serviceapiobra/ServiceApiObraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicon.api.serviceapiobra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiObraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
